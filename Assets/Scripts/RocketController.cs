789f5ff91c125740a1996b6047c61d0c2236abb0
Md Rohidul Islam <rohidul677@gmail.com>
2020-01-05 02:51:30 +0600

    Rocket Controller done

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpForce = 10f;
+
+    private void Update()
+    {
+        float velY = rb.velocity.y;
+        float ang = Mathf.Atan2(velY, 10) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, ang);
+
+        if (Input.GetMouseButtonDown(0) | Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(0, jumpForce, 0);
+            
+        }
+    }
+}
